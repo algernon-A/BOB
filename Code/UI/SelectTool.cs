1dbb04c1c46b3f858bb7974c5e23e498a4a02d34
algernon-A <aeneas@aloku.net>
2020-09-21 19:36:02 +1000

    Add ignore flag overrides to select tool.

--- Code/UI/SelectTool.cs
+++ Code/UI/SelectTool.cs
@@ -15,6 +15,13 @@ namespace BOB
 		private CursorInfo darkCursor;
 
 
+		public enum Mode
+		{
+			Select,
+			NodeOrSegment,
+			Building,
+			PropOrTree
+		}
 		/// <summary>
 		/// Instance reference.
 		/// </summary>
@@ -31,6 +38,27 @@ namespace BOB
 			lightCursor = FileUtils.LoadCursor("bob_cursor_light.png");
 			darkCursor = FileUtils.LoadCursor("bob_cursor_dark.png");
 			m_cursor = darkCursor;
+		}
+
+		// Ignore nodes, citizens, disasters, districts, transport lines, and vehicles.
+		public override NetNode.Flags GetNodeIgnoreFlags() => NetNode.Flags.All;
+		public override CitizenInstance.Flags GetCitizenIgnoreFlags() => CitizenInstance.Flags.All;
+		public override DisasterData.Flags GetDisasterIgnoreFlags() => DisasterData.Flags.All;
+		public override District.Flags GetDistrictIgnoreFlags() => District.Flags.All;
+		public override TransportLine.Flags GetTransportIgnoreFlags() => TransportLine.Flags.All;
+		public override VehicleParked.Flags GetParkedVehicleIgnoreFlags() => VehicleParked.Flags.All;
+		public override Vehicle.Flags GetVehicleIgnoreFlags() => Vehicle.Flags.LeftHandDrive | Vehicle.Flags.Created | Vehicle.Flags.Deleted | Vehicle.Flags.Spawned | Vehicle.Flags.Inverted | Vehicle.Flags.TransferToTarget | Vehicle.Flags.TransferToSource | Vehicle.Flags.Emergency1 | Vehicle.Flags.Emergency2 | Vehicle.Flags.WaitingPath | Vehicle.Flags.Stopped | Vehicle.Flags.Leaving | Vehicle.Flags.Arriving | Vehicle.Flags.Reversed | Vehicle.Flags.TakingOff | Vehicle.Flags.Flying | Vehicle.Flags.Landing | Vehicle.Flags.WaitingSpace | Vehicle.Flags.WaitingCargo | Vehicle.Flags.GoingBack | Vehicle.Flags.WaitingTarget | Vehicle.Flags.Importing | Vehicle.Flags.Exporting | Vehicle.Flags.Parking | Vehicle.Flags.CustomName | Vehicle.Flags.OnGravel | Vehicle.Flags.WaitingLoading | Vehicle.Flags.Congestion | Vehicle.Flags.DummyTraffic | Vehicle.Flags.Underground | Vehicle.Flags.Transition | Vehicle.Flags.InsideBuilding;
+
+
+		/// <summary>
+		/// Called by the game.  Sets which network segments are ignored by the tool (always returns none, i.e. all segments are selectable by the tool).
+		/// </summary>
+		/// <param name="nameOnly">Always set to false</param>
+		/// <returns>NetSegment.Flags.None</returns>
+		public override NetSegment.Flags GetSegmentIgnoreFlags(out bool nameOnly)
+		{
+			nameOnly = false;
+			return NetSegment.Flags.None;
 		}
 
 
@@ -80,8 +108,17 @@ namespace BOB
 			}
 			else
 			{
-				// No building hovered; set the cursor to the dark cursor.
-				m_cursor = darkCursor;
+				ushort segment = m_hoverInstance.NetSegment;
+				// Try to get a hovered network instance.
+				if (segment != 0)
+				{
+					m_cursor = lightCursor;
+				}
+				else
+				{
+					// No building or network hovered; set the cursor to the dark cursor.
+					m_cursor = darkCursor;
+				}
 			}
 		}
 	}
