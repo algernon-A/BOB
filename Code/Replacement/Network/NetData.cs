dc7b10ac49f5bdce6675f4100dbec154b02a39eb
algernon-A <aeneas@aloku.net>
2022-02-21 00:22:10 +1100

    Rebuild group renders directly on network update.

--- Code/Replacement/Network/NetData.cs
+++ Code/Replacement/Network/NetData.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using ColossalFramework;
+using UnityEngine;
 
 
 namespace BOB
@@ -36,21 +37,48 @@ namespace BOB
         /// </summary>
         internal static void Update()
         {
+            HashSet<KeyValuePair<int, int>> groupHash = new HashSet<KeyValuePair<int, int>>();
+
+            // Local references.
+            NetManager netManager = Singleton<NetManager>.instance;
+            NetSegment[] segments = netManager.m_segments.m_buffer;
+            NetNode[] nodes = netManager.m_nodes.m_buffer;
+
             // Need to do this for each segment instance, so iterate through all segments.
-            NetSegment[] segments = NetManager.instance.m_segments.m_buffer;
             for (ushort i = 0; i < segments.Length; ++i)
             {
-                // Local reference.
-                NetSegment segment = segments[i];
+				// Check that this is a valid network in the dirty list.
+				if (segments[i].m_flags != NetSegment.Flags.None && DirtyList.Contains(segments[i].Info))
+				{
+                    // Caclulate segment render group.
+                    ushort startNode = segments[i].m_startNode;
+                    ushort endNode = segments[i].m_endNode;
+                    Vector3 position = nodes[startNode].m_position;
+                    Vector3 position2 = nodes[endNode].m_position;
+                    Vector3 vector = (position + position2) * 0.5f;
+                    int num = Mathf.Clamp((int)(vector.x / 64f + 135f), 0, 269);
+                    int num2 = Mathf.Clamp((int)(vector.z / 64f + 135f), 0, 269);
+                    int x = num * 45 / 270;
+                    int z = num2 * 45 / 270;
 
-                // Check that this is a valid network in the dirty list.
-                if (segment.m_flags != NetSegment.Flags.None && DirtyList.Contains(segment.Info))
-                {
-                    // Match - update segment render via simulation thread, creating local buildingID reference to avoid race condition.
-                    ushort segmentID = i;
-                    Singleton<SimulationManager>.instance.AddAction(delegate { Singleton<NetManager>.instance.UpdateSegmentRenderer(segmentID, true); });
+                    // Add render group coordinates to hashlist (ignore if already there).
+                    groupHash.Add(new KeyValuePair<int, int>(x, z));
                 }
             }
+
+            // Update render groups via simulation thread.
+            Singleton<SimulationManager>.instance.AddAction(delegate
+            {
+                // Iterate through each key in group.
+                foreach (KeyValuePair<int, int> keyPair in groupHash)
+                {
+                    // Update group render (all 32 layers, since we've got all kinds of mismatches with replacements).
+                    for (int i = 0; i < 32; ++i)
+                    {
+                        Singleton<RenderManager>.instance.UpdateGroup(keyPair.Key, keyPair.Value, i);
+                    }
+                }
+            });
 
             // Recalculate any Network Skins 2 applied skins.
             ModUtils.NS2Recalculate();
@@ -58,5 +86,5 @@ namespace BOB
             // Clear dirty prefabs list.
             DirtyList.Clear();
         }
-    }
+	}
 }
